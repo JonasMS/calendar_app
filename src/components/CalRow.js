83775e1b983b318567a0b325843f5ed86f620af4
JonasMS <jonas.sota@gmail.com>
2016-09-11 14:18:04 -0700

    FEAT - Style Time Column, WIP

--- src/components/CalRow.js
+++ src/components/CalRow.js
@@ -2,11 +2,24 @@ import React from "react";
 import timeToString from "../modules/"
 
 
-const CalRow = ({time, id, addRowRef}) => (
-  <tr data-time={id}>
-    <td className="time-field">{time}</td>
-    <td className="event-field" ref={addRowRef}></td>
-  </tr>
-);
+const CalRow = ({time, id, addRowRef}) => {
+  const timeProp = time.split(" ");
+  return (
+    <tr data-time={id}>
+      <td className="time-field">
+        <div className="time-container">
+          <div className={timeProp[1] ? "primary" : "secondary"}>{timeProp[0]}</div>
+          {
+            timeProp[1] ?
+              <div className="meridiem">{timeProp[1]}</div>
+              :
+              ""
+          }
+        </div>
+      </td>
+      <td className="event-field" ref={addRowRef}></td>
+    </tr>
+  )
+};
 
 export default CalRow;
